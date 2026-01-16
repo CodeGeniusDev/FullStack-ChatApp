f2c256821e9d9d5b99f2c7754dac3f6d4176565b
Abdullah Abbad <codegenius.inc@gmail.com>
2026-01-16 17:26:06 +0500

    fix some issues

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" 
+    ? "http://localhost:5002/api" 
+    : "/api",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
